7b11dd996e98c7528e0036f10d8a88db28526448
Bane <bane.baki.1997@gmail.com>
2022-03-07 22:02:32 +0100

    Adding new files and refactoring
    Added new projects to work on (lvl2)
    Slight code refactoring
    Started work on calculator app

---
+++ Calculator/script.js
@@ -0,0 +1,24 @@
+const getIt = (x) => {return document.getElementById(x)};
+// getting all elements
+	const num1 = getIt("num1");
+	const num2 = getIt("num2");
+	const num3 = getIt("num3");
+	const num4 = getIt("num4");
+	const num5 = getIt("num5");
+	const num6 = getIt("num6");
+	const num7 = getIt("num7");
+	const num8 = getIt("num8");
+	const num9 = getIt("num9");
+	const num0 = getIt("num0");
+
+	const add = getIt("add");
+	const sub = getIt("sub");
+	const multi = getIt("multi");
+	const equal = getIt("equal");
+	const dot = getIt("dot");
+
+	const result = getIt("result");
+	const clearCurrent = getIt("clearCurrent");
+	const clearAll = getIt("clearAll");
+//
+

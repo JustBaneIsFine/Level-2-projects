50b1876e20f660243562f25fcdaf1a7081aa0e01
Bane <bane.baki.1997@gmail.com>
2022-03-16 21:54:33 +0100

    app 40% done

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,6 +1,6 @@
 // to do:
 // clearRect
-// draw snake food grid
+// update snake food grid
 // snake.move , snake.changeDirection , snake.addLenght , snake.death
 // food.newPosition 
 // etc..
@@ -10,9 +10,17 @@ const canvas = document.getElementById("canvas");
 var snake;
 var food;
 var score = 0;
-
+var grid = [];
 
 window.onload = () => {gameArea.start()}
+
+														// blockSize = 20;
+														// xBlockNumber = 25;	//vertical
+														// yBlockNumber = 35;	//horizontal
+
+														// numberOfBlocks = 875;
+
+
 
 
 var gameArea = 
@@ -28,7 +36,8 @@ var gameArea =
 			this.context = this.canvas.getContext("2d");
 			this.canvas.style = "background-color: white";
 			document.body.childNodes[3].insertBefore(this.canvas, document.body.childNodes[3].childNodes[0]);
-			this.gameInterval = setInterval(updateGameArea,1000/120);
+			this.gameInterval = setInterval(updateGameArea,1000/30);
+			grid.push()
 
 				window.addEventListener("keydown", function(e)
 					{
@@ -55,6 +64,8 @@ var gameArea =
 			}
 	};
 
+ctx = gameArea.context;
+
 function snake(width, height,color,x,y)
 	{
 		this.width = width;
@@ -66,39 +77,85 @@ function snake(width, height,color,x,y)
 		this.body = [];
 		this.bodyCount;
 		this.direction = "right";
-		this.lenght = 1;
-		this.move = setInterval(moveHandler, speed*1000);
+		this.length = 1;
+		this.moveHandler = setInterval(moveHandler, speed*1000);
+		this.isDead = false;
 
-		this.draw = function()
+		this.update = function()
 			{
-				drawSnake(this.body);
+				//----For each body part, do this..
+				ctx.fillStyle = this.color;
+				ctx.fillRect(this.x, this.y, this.width, this.height)
 			}
 		this.moveHandler = function()
 			{
-				// if body === bodyCount ---> moveFull
-				// else if body != bodyCount ---> moveHead (once)
+
+				if (!isDead && bodyCount === body.length)
+					{
+						function moveFull()
+							{
+								// move in the this.direction.
+								// if direction right, snake.body add to start (current x position + 20(one block) + current y position)
+								// remove last from snake.body
+							}
+					} 
+				else if (!isDead && bodyCount != body.length)
+					{
+
+						function moveHead()
+							{
+								// same as moveFull except last is not removed until body.length and bodyCount match up (has to be on the move interval!)
+								// we make one move, then update, on next move() iteration, it will do the same, until bodyCount === body.length.
+							}
+					}
+				else 
+					{
+						// do not move
+					}
+					
+					
+
+
+
+			
+				
 			}
-		this.moveFull = function()
-			{
-				// move in the this.direction.
-				// if direction right, snake.body add to start (current x position + 20(one block) + current y position)
-				// remove last from snake.body
-			}
-		this.moveHead = function()
-			{
-				// same as moveFull except last is not removed until body.length and bodyCount match up (has to be on the move interval!)
-			}
+		
 		this.addLength = function()
 			{
+
 				//when food and body[0] are in the same grid
 				//we increase bodyCount by x number..
 				// then the moveHead function will run until it's done and then the moveFull continues..
 
 			}
+			
 		this.changeDirection = function (dir) 
 			{
+				switch(dir){
+					case "left":
+					if (!direction === "right"){}
+					direction = dir;
+					break;
+					case "right":
+					if (!direction === "left"){}
+					direction = dir;
+					break;
+					case "up";
+					if (!direction === "down"){}
+					direction = dir;
+					break;
+					case "down";
+					if (!direction === "up"){}
+					direction = dir;
+					break;
+				}
 				 // if new direction right, left etc.
 				 // direction = right,left etc.
+			}
+		this.newSnake = function()
+			{
+				//reset all snake stats which starts the game again
 			}
 	};
 
@@ -109,20 +166,23 @@ function food(width, height,color,x,y)
 		this.x = x;
 		this.y = y;
 		this.color = color;
-		this.draw = function()
-			{
-				drawFood(this.body);
+
+		this.update = function()
+			{	
+						ctx.fillStyle = this.color;
+						ctx.fillRect(this.x, this.y, this.width, this.height)
 			}
 		this.newPosition = function()
 			{
-				newFood();
+				this.y = (Math.floor(Math.random()*(25)))*20;
+				this.x = (Math.floor(Math.random()*(35)))*20;
 			}
 
 	};
 
-function drawScore()
+function updateScore()
 	{
-		ctx = gameArea.context;
+		
 		ctx.font = "20px Arial";
 		ctx.fillText(score, 4*800/8-5,20);
 	}
@@ -130,5 +190,27 @@ function drawScore()
 function updateGameArea()
 	{
 
+		//update the same as snake speed interval?
+
+
+
+
+
+
+
+
+
+
+
+		if (checkPositions(snake.x,snake.y,food.x,food.y))
+			{
+				snake.addLength();
+				food.newPosition();
+			}
+		
 	}
 
+function checkPositions(ax,ay,bx,by)
+	{
+		ax === bx && ay === by ? true : false;
+	}

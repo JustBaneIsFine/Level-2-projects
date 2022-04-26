f437fb52e7a180bd27cf3b13df545976432d9837
Bane <bane.baki.1997@gmail.com>
2022-04-26 20:43:40 +0200

    Fixed and refactored the code, more work to be done

--- Web Scraper/script.js
+++ Web Scraper/script.js
@@ -10,71 +10,59 @@ var success = false;
 var browserClosed = false;
 var contentLoaded = false;
 var ran = false;
+
 loadPage(URLkupujem);
-
-
-
-
-
 
 
 
 async function loadPage (URL) 
 	{
-		
+		//while the page throws error or is not loaded properly, try again..
+		try{
 		while(!success)
 			{
 				const browser = await puppeteer.launch({headless:false});
 				const page = await browser.newPage();
 				await page.setRequestInterception(true);
-				
 
-
-
-				page.waitForSelector('.categoryTitle').then(() => {contentLoaded = true;console.log("ITS HERE <<<<<<<<<<<<<<<<<<<<<<<<<<<")})
-				//when selector shows up, then do what needs to be done.
-				// //page.waitForSelector('.adListContainer').then(async function()
-				// 	{
-				// 		contentLoaded = true;
-				 		
-				// 			// do what has to be done with the data..
-				// 			//then exit..
-				// 			 try {
-
-				// 			// 	// _____________CODE SNIPET________________
-
-				// 			// 	 //  	const array = await page.evaluate(() => {
-				// 			// 	 //  		console.log("WORKING <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-				// 			// 		//  	return document.getElementsByClassName("categoryTitle"); //this get's all elements with this class name and returns a html collection
-				// 			// 		// })
-
-								
-				// 			// 		const array = await page.$$("categoryTitle");
-				// 			// 		//console.log(array[0].childNodes[3].innerText);
-
-									
-
-				// 			// 	   var b = [];
-				// 			// 	   for (i = 0;i < array.length;i++){b.push(array[i].childNodes[3].innerText)};
-				// 			// 		console.log(b + "yes yes");   
-
-				// 			// 	// _____________CODE SNIPET________________
-				// 				//await browser.close();
-								
-				// 				} catch(e) {
-				// 					console.log(e);
-				// 				}
-							
-				// 	}).catch((e)=>console.log(e));
-
-				// handle requests, do not load any "heavy" or "slow" stuff like images etc.
-				if(!contentLoaded){
-					try {
+					// when categoryTitle shows up, do this
+					page.waitForSelector('.categoryTitle').then(() => 
+						{
 							success = true;
+							contentLoaded = true;
 
-								page.on('request', request => {
+									//here we do what we have to with the code
+									(async function()
+										{
+											const text = await page.evaluate(() => {
+										   	var a = document.getElementsByClassName("categoryTitle");
+										   	var array = [];
+										   	for (i = 0;i<a.length;i++)
+										   		{
+										   			array.push(a[i].childNodes[3].innerText);
+										   		}
 
-								  if (
+										   	return array; 
+										})
+
+										console.log(text);
+										await browser.close();
+										}
+
+									)();	
+
+							
+
+
+						});
+
+					
+					
+					//intercept page requests.
+						page.on('request', request => {
+
+								if (!contentLoaded){
+								  if ( 
 								  	request.resourceType() === 'image' || 
 								  	request.resourceType() ==='imageset' ||
 								  	request.resourceType() ==='media'||
@@ -84,124 +72,31 @@ async function loadPage (URL)
 								  	request.resourceType() === 'sub_frame'||
 								  	request.resourceType() === 'xmlhttprequest'
 								  	)
-								  {
-								  		request.respond({
-							          	status: 200,
-							          	body: "foo"
-							       		 })
-
-
-								  	// request.abort();
-								  	// request._interceptionHandled = false;
-								  	// request.respond({status: 200,body:"foo"});
-								    
-								    console.log("aborted something");
-								  }
+										{
+										 	request.respond({
+									        status: 200,
+									        body: "foo"
+									     	})
+										    
+										 	console.log("aborted request");
+										}
 								  else
-								  {
-								  	console.log("it continued");
-								    request.continue();
-								  }
+									  {
+									  	console.log("allowed request");
+									    request.continue();
+									  }	
+									}
+								
+						})
 
-								  // THIS WORKS IN CONSOLE IN BROWSER:
-								  // array = document.getElementsByClassName("categoryTitle") ->>> returns an HTML collection
-
-								  // array[0].childNodes[3].innerText -->> gets the text inside..
-
-
-
-
-
-
-						// try this  outside the page.waitForSelector to get the element, because you probably can't 
-						// use the page.$$  inside the page.waitForSelector
-						if (contentLoaded && !ran){
-							ran = true;
-							console.log("content is loaded ran");
-
-								(async function()
-								{
-									
-									///_____________
-									const text = await page.evaluate(() => {
-								   	console.log(document.getElementsByClassName("categoryTitle")); //this get's all elements with this class name and returns a html collection
-									})
-									
-									//_____________________CODE TO EDIT AND TRY OUT_____________________
-									// https://github.com/puppeteer/puppeteer/issues/4852
-									//_____________________CODE TO EDIT AND TRY OUT_____________________
-
-
-
-
-									// const text = await page.evaluate(()=>{
-									// 	var elements = Array.from(document.querySelectorAll(".categoryTitle"));
-									// 	var links = elements.map(element => {
-								 //            return element.childNodes[3].innerText;
-								 //         console.log(links)
-								 //        })
-								 //        return links;
-										//var x = document.querySelectorAll(".categoryTitle").forEach(x => array.push(x.childNodes[3].innerText));
-										//return array;
-										//for (i = 0;i < x.length;i++){array.push(x[i].childNodes[3].innerText)};
-											
-									
-
-
-									// const text = await page.evaluate(() => {
-
-									// return array = page.$$("categoryTitle"); //this get's all elements with this class name and returns a html collection
-									// 	console.log(array[0].childNodes[3].innerText);
-									// 	})
-
-									 //   var b = [];
-									 //   for (i = 0;i < array.length;i++){b.push(array[i].childNodes[3].innerText)};
-										// console.log(b);   	
-
-
-									  console.log("THIS IS THE CONTENT");
-									///______________
-
-
-									// var array = [];
-									// const abc = await page.evaluate(() => Array.from(document.getElementsByClassName('categoryTitle'), e => e.childNodes[3].innerText));
-									// //const array = await page.$$eval('.categoryTitle', element => element);
-									// console.log(abc.length);
-								})();
-							}
-
-
-								});
-
-								await page.goto(URLkupujem);
-								console.log("success");
-
-
-
-						} 
-					catch(e) 
-						{
-							console.log(e + "ENDING ERROR <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-							await browser.close();
-						}
-				}
+					await page.goto(URLkupujem);
 			}
-	}
+		}
+		catch(e){console.log(e + " Expected navigation error")}
+
+	};
 
 
-
-
-
-
-
-
-	 
-
-		
-
-
-
-// })();
 
 
 const getRawData = (URL) => 

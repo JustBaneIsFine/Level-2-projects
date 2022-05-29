8d734b61576db1e4dd66f0220f84938817ce163f
Bane <bane.baki.1997@gmail.com>
2022-05-29 14:54:39 +0200

    Fixed page loading and data handling
    -page loading inconsistently and returning less data than it should

--- Web Scraper/script.js
+++ Web Scraper/script.js
@@ -24,26 +24,16 @@ const URLpolovni = "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=
 var exportDone = false;
 var data;
 var theList = [];
-var pageNum = 10;
+var pageNum =12;
 var pageCounter = 0;
 var pagesPerCycle = 5;
 var carModel;
 var carMake;
 var carYear;
 var sheetNum = 0;
-// Part 1: Input the desired car details to compare: car make, car model, car year;
-// This will be daone on sites themselves, and then the user will grab the link and input it into the URL fields above..
-// Or it can be done trough a server...
+var amount = pagesPerCycle;
+var displayCount = 0;
 
-// Part 2: The user sets up the values they want to get from that website, as well as how many pages per cycle etc.
-
-// Part 3: The user sets up their spreadSheet, and the names/columns etc.
-
-// Part 4: The user starts the program...
-
-// Part 5: The program grabs the urls, and the pageNum amounts and begins to extract data and everything it needs..
-
-// Part 6: Data is extracted and the user get's the data
 
 
 
@@ -55,11 +45,12 @@ async function loadWebsites()
 	{
 		sheetNum = 0;
 		await mainHandlerKupujem();
-		// now i can await for the export function to complete...
-		console.log("I should have finished everything now.")
 		data = undefined;
 		theList = [];
 		sheetNum = 1;
+		pageCounter = 0;
+		amount = pagesPerCycle;
+		displayCount = 0;
 		await mainHandlerPolovni();
 		console.log("DONE!");
 	}
@@ -70,15 +61,14 @@ async function mainHandlerKupujem()
 		console.log("KUPUJEM LOADING")
 		while(pageCounter<pageNum)
 			{
+
 				var store = [];
-				var amount = pagesPerCycle;
 				console.log("_________________Loading Pages_________________");
 				
-				checkPageProgress();
+				checkPageProgress(); 
 
 				for (i=0;i<amount;i++)
 					{
-
 						var url = URLkupujem.concat("/"+(pageCounter+1));
 						pageCounter++;
 
@@ -89,7 +79,8 @@ async function mainHandlerKupujem()
 
 						store[i].then(x => 
 						{
-							console.log("_________________got page_________________");
+							displayCount++;
+							console.log("_________________got page_________________", `${displayCount}/${pageNum}`);
 
 							if(x != false)
 								{
@@ -100,14 +91,6 @@ async function mainHandlerKupujem()
 						.catch((e)=>console.log(e+"Failed to get data from a page"));
 
 					}
-					// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<THIS IS WHERE YOU LEFT OF<<<<<<<<<<<<<<<<<<<<<<<
-					// So with promise.all and doing it like this, data1,data2 it works perfectly..
-					// with only data,data it works strange, probably because i am creating multiple
-					// promises with the same name and awaiting them...
-
-					//so i need to figure out a way, how to create variables with a function dynamicaly,
-					//and then i can send those variables(promieses) to an array, and promise.all(array) that array
-					// and that should work 
 
 				await Promise.all(store)
 				
@@ -122,7 +105,7 @@ async function mainHandlerKupujem()
 			// what if i wrap it all in another async and await
 
 			await handleData();
-			pageCounter = 0;
+
 	}
 
 async function mainHandlerPolovni()
@@ -132,12 +115,13 @@ async function mainHandlerPolovni()
 		while(pageCounter<pageNum)
 			{
 				var store = [];
-				var amount = pagesPerCycle;
+				
 				console.log("_________________Loading Pages_________________")
 				checkPageProgress();
 
 				for (i=0;i<amount;i++)
 					{
+
 						var url = `https://www.polovniautomobili.com/auto-oglasi/pretraga?page=${pageCounter+1}&sort=basic&brand=opel`;
 						
 						pageCounter++;
@@ -153,10 +137,8 @@ async function mainHandlerPolovni()
 						.catch((e)=>console.log(e+"Failed to get data from a page"));
 					}
 				await Promise.all(store);
-
 			}
 		await handleData();
-		pageCounter = 0;
 	}
 
 
@@ -176,89 +158,98 @@ async function loadPage (URL,choice)
 					if (choice === "kupujem")
 						{
 							data = page.waitForSelector('.adName',{timeout: 15000})
-								.then(()=>
+							.then(()=>
+							{
+								return (async function()		
+								{
+								try
 									{
-										return (async function()
-											
+										const text = await page.evaluate(()=>
+										{
+											return (async function() 
 											{
-												try
+										
+												var array = [];
+
+												var promiseDom = new Promise((resolve,reject)=>
 													{
-														const text = await page.evaluate(()=>{
-
-															var listOfNames = document.getElementsByClassName("adName");
-															var array = [];
-
-															for (i=0;i<listOfNames.length-1;i++)
-																{
-																		var parent = listOfNames[i].parentElement.parentElement.parentElement.parentElement;
-																		var name = parent.querySelector(".adName").innerText;
+														document.addEventListener('DOMContentLoaded',resolve);
+													});
 
 
-																		var price = parent.querySelector(".adPrice").innerText;
+												await promiseDom;
+												
+												var listOfNames = document.getElementsByClassName("adName");
 
-																		if (
-																			!price.includes("Dogovor") &&
-																			!price.includes("Kontakt") &&
-																			!price.includes("Pozvati") &&
-																			!price.includes("Kupujem")
-																			)
-																		{
-																			price = price
-																			.replaceAll(".","")
-																			.replaceAll("€","")
-																			.replaceAll(" ","")
-																			.replaceAll(",","");
+												for (i=0;i<listOfNames.length;i++)
+													{
+															var parent = listOfNames[i].parentElement.parentElement.parentElement.parentElement;
+															var name = parent.querySelector(".adName").innerText;
 
-																			if(!price.includes("din"))
-																				{
-																					price = price.slice(0,-2);
-																				}
-																		}
 
-																		var href =  parent.querySelector(".adName").href;
+															var price = parent.querySelector(".adPrice").innerText;
 
-																		
-																		var description = parent.querySelector(".adDescription").innerText;
-																		description = description.split(',');
+															if (
+																!price.includes("Dogovor") &&
+																!price.includes("Kontakt") &&
+																!price.includes("Pozvati") &&
+																!price.includes("Kupujem")
+																)
+															{
+																price = price
+																.replaceAll(".","")
+																.replaceAll("€","")
+																.replaceAll(" ","")
+																.replaceAll(",","");
 
-																		var year = description[0];
-																		var fuel =  description[3].split('.')[0];
-																		var cc =  description[2].replaceAll("cm3","");
-																		var km =  description[1].replaceAll("km","").replaceAll(".",",");
+																if(!price.includes("din"))
+																	{
+																		price = price.slice(0,-2);
+																	}
+															}
 
-																		name = `\"${name}\"`;
-																		href = `\"${href}\"`;
+															var href =  parent.querySelector(".adName").href;
 
-																		var adObj = {
-																			"Car Name":`=HYPERLINK(${href},${name})`,
-																			"Car Price": price,
-																			"Car Year":year,
-																			"Car Fuel":fuel,
-																			"Car KM":km,
-																			"Car CC":cc,
-																			}
+															
+															var description = parent.querySelector(".adDescription").innerText;
+															description = description.split(',');
 
-																		array.push(adObj);
+															var year = description[0];
+															var fuel =  description[3].split('.')[0];
+															var cc =  description[2].replaceAll("cm3","");
+															var km =  description[1].replaceAll("km","").replaceAll(".",",");
+
+															name = `\"${name}\"`;
+															href = `\"${href}\"`;
+
+															var adObj = {
+																"Car Name":`=HYPERLINK(${href},${name})`,
+																"Car Price": price,
+																"Car Year":year,
+																"Car Fuel":fuel,
+																"Car KM":km,
+																"Car CC":cc,
 																}
 
-															return array; // text is now array..
-														})
+															array.push(adObj);
+													} //for closing
 
-													//console.log(text, "<<<<<< THIS IS TEXT")
-													//data = text;
-													//console.log(data, "<<< THIS IS DATA");
+													return array;
+												})();
 
-													await browser.close();
-													contentLoaded = true;
-													return text;
-													}
+													
+													
+											});
+										await browser.close();
+										contentLoaded = true;
+										return text;
+												
+									}
+									catch(e){console.log(e,"error that we get")};
 
-												catch(e){console.log(e,"error that we get")}
-												return text;
-											}
-										)();
-									})
-								.catch((e)=>{console.log(e+ "expected error"); browser.close();return false;})
+								})();
+							})
+							.catch((e)=>{console.log(e+ "expected error"); browser.close();return false;})
 						} 
 					else if (choice === "polovni")
 
@@ -278,10 +269,23 @@ async function loadPage (URL,choice)
 																//adName for kupujemprodajem
 																//textContentHolder for polovni automobili
 
-																var listOfNames = document.getElementsByClassName("textContentHolder");
-																var array = [];
+																return (async function() {
+																	var array = []
 
-																for (i=0;i<listOfNames.length-1;i++)
+
+
+																	var promiseDom = new Promise((resolve,reject)=>
+																	{
+																		document.addEventListener('DOMContentLoaded',resolve);
+																	});
+
+
+																	await promiseDom;
+																	//____new
+
+																	var listOfNames = document.getElementsByClassName("textContentHolder");
+
+																	for (i=0;i<listOfNames.length;i++)
 																	{
 																		var parent = listOfNames[i].parentElement;
 
@@ -351,196 +355,29 @@ async function loadPage (URL,choice)
 																		array.push(adObj);
 																	}
 
-																return array; // text is now array..
+																 // text is now array..
+																
+																	//____
 
-															})
+																	return array;
+																})();
 
+
+
+
+															});
 														await browser.close();
 														contentLoaded = true;
 														return text;
 														}
-
 													catch(e){console.log(e,"error that we get")}
-													return text;
 												}
 											)();
 										})
 									.catch((e)=>{console.log(e+ "expected error"); browser.close();return false;})
 							}
 
-					// data = page.waitForSelector('.adName',{timeout: 15000})
-					// 	.then(()=>
-					// 		{
-					// 			//when .adName shows up, do this:
-
-					// 			return (async function()
-									
-					// 				{
-					// 					try
-					// 						{
-					// 							const text = await page.evaluate(()=>{
-					// 								//or i could do, for each of these, get name,price and href.
-
-					// 								//changeThis
-					// 								//adName for kupujemprodajem
-					// 								//textContentHolder for polovni automobili
-
-					// 								var listOfNames = document.getElementsByClassName("adName");
-					// 								var array = [];
-
-					// 								for (i=0;i<listOfNames.length-1;i++)
-					// 									{
-					// 										//changeThis
-					// 										//used for kupujem 
-					// 										//_______
-
-					// 											var parent = listOfNames[i].parentElement.parentElement.parentElement.parentElement;
-					// 											var name = parent.querySelector(".adName").innerText;
-
-
-					// 											var price = parent.querySelector(".adPrice").innerText;
-
-					// 											if (
-					// 												!price.includes("Dogovor") &&
-					// 												!price.includes("Kontakt") &&
-					// 												!price.includes("Pozvati") &&
-					// 												!price.includes("Kupujem")
-					// 												)
-					// 											{
-					// 												price = price
-					// 												.replaceAll(".","")
-					// 												.replaceAll("€","")
-					// 												.replaceAll(" ","")
-					// 												.replaceAll(",","");
-
-					// 												if(!price.includes("din"))
-					// 													{
-					// 														price = price.slice(0,-2);
-					// 													}
-					// 											}
-
-					// 											var href =  parent.querySelector(".adName").href;
-
-																
-					// 											var description = parent.querySelector(".adDescription").innerText;
-					// 											description = description.split(',');
-
-					// 											var year = description[0];
-					// 											var fuel =  description[3].split('.')[0];
-					// 											var cc =  description[2].replaceAll("cm3","");
-					// 											var km =  description[1].replaceAll("km","").replaceAll(".",",");
-
-					// 											name = `\"${name}\"`;
-					// 											href = `\"${href}\"`;
-
-					// 											var adObj = {
-					// 												"Car Name":`=HYPERLINK(${href},${name})`,
-					// 												"Car Price": price,
-					// 												"Car Year":year,
-					// 												"Car Fuel":fuel,
-					// 												"Car KM":km,
-					// 												"Car CC":cc,
-					// 												}
-
-					// 											array.push(adObj);
-
-					// 										//_______
-
-					// 										//used for polovni
-					// 										//_______
-
-					// 											// var parent = listOfNames[i].parentElement;
-
-					// 											// var name = parent.querySelector(".ga-title").innerText;
-					// 											// var priceDiscount = parent.querySelector(".price").querySelector(".priceDiscount");
-					// 											// var price = parent.querySelector(".price").innerText;
-
-					// 											// if(priceDiscount != null)
-					// 											// 	{
-					// 											// 		price = priceDiscount.innerText;
-					// 											// 	}
-
-					// 											// if(price.includes("+"))
-					// 											// 	{	
-					// 											// 		var b;
-					// 											// 		b = price.split("+");
-					// 											// 		price = b[0];
-					// 											// 	}
-					// 											// else if(price.includes("\n"))
-					// 											// 	{
-					// 											// 		var b;
-					// 											// 		b = price.split("\n");
-					// 											// 		price = b[1];
-					// 											// 	};
-
-					// 											// if (
-					// 											// 	!price.includes("Dogovor") &&
-					// 											// 	!price.includes("Kontakt") &&
-					// 											// 	!price.includes("Pozvati")
-					// 											// 	)
-					// 											// {
-					// 											// 	price = price
-					// 											// 	
-					// 											// 	.replaceAll(".","")
-					// 											// 	.replaceAll("€","")
-					// 											// 	.replaceAll(" ","")
-					// 											// 	.replaceAll(",","").slice(0,-2);
-
-					// 												// if(!price.includes("din"))
-					// 												// 	{
-					// 												// 		price = price.slice(0,-2);
-					// 												// 	}
-					// 											// }
-																
-
-					// 											// var href = parent.querySelector(".ga-title").href;
-																
-					// 											// //access info trough children  description.children[0] etc..
-					// 											// var description = parent.querySelector(".info");
-
-					// 											// var year = description.children[0].innerText.split('\n')[0].split('.')[0];
-					// 											// var fuel = description.children[0].innerText.split('\n')[1].split("|")[0];
-					// 											// var cc =  description.children[0].innerText.split('\n')[1].split("|")[1].replaceAll("cm3","");
-					// 											// var km =  description.children[1].innerText.split("\n")[0].replaceAll(".",",").replaceAll("km","");
-
-					// 											// name = `\"${name}\"`;
-					// 											// href = `\"${href}\"`;
-
-					// 											// var adObj = {
-					// 											// 	"Car Name":`=HYPERLINK(${href},${name})`,
-					// 											// 	"Car Price": price,
-					// 											// 	"Car Year":year,
-					// 											// 	"Car Fuel":fuel,
-					// 											// 	"Car KM":km,
-					// 											// 	"Car CC":cc,
-					// 											// 	}
-
-					// 											// array.push(adObj);
-
-					// 										//_______
-
-					// 									}
-
-					// 								return array; // text is now array..
-
-					// 							})
-
-					// 						//console.log(text, "<<<<<< THIS IS TEXT")
-					// 						//data = text;
-					// 						//console.log(data, "<<< THIS IS DATA");
-
-					// 						await browser.close();
-					// 						contentLoaded = true;
-					// 						return text;
-					// 						}
-
-					// 					catch(e){console.log(e,"error that we get")}
-					// 					return text;
-					// 				}
-					// 			)();
-					// 		})
-					// 	.catch((e)=>{console.log(e+ "expected error"); browser.close();return false;})
-
+					
 
 					//intercept page requests
 					page.on('request',request => {
@@ -595,7 +432,7 @@ async function loadHandler(URL,choice)
 						if (data === undefined || data === false)
 							{
 								fail = true; data = false;
-								console.log("failed")
+								console.log("failed");
 							}
 						else{
 								fail = false;
@@ -633,6 +470,7 @@ async function exportDataToExcel(data)
 		})
 		console.log("_________________ploting rows_________________")
 		console.log("_________________adding rows_________________")
+
 		await sheet.addRows(data);
 		console.log("__________________DONE!__________________")
 
@@ -651,15 +489,16 @@ function writeToFile(content)
 
 function checkPageProgress()
 			{
-				if(pageNum-pageCounter < pagesPerCycle)
+				if((pageNum-pageCounter) < pagesPerCycle) //7-5 < 5
 					{
 						amount = pageNum-pageCounter;
+
 					}
 			}
 
 async function handleData(x)
 	{
-		console.log("Handling data", theList.length, pageNum);
+		console.log("__________________Handling data__________________");
 		//the below code should be the same for both websites
 		var content = [];
 
@@ -697,8 +536,7 @@ async function handleData(x)
 					//content is now usable as array of objects [{name-name,price-price},{etc}]
 						await exportDataToExcel(content);
 
-						
-						//content.forEach(x=>console.log(`${x.name}\n`));							
+												
 
 					//writeToFile(content)
 				}
